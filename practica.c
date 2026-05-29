a7d57e80cd0e8f00ba659816bfe11b7ce06717b1
0milagros0 <109671392+0milagros0@users.noreply.github.com>
2026-05-29 13:11:22 -0300

    ejercicios en c

--- practica.c
+++ practica.c
@@ -73,6 +73,71 @@ for (int i = 0; i < 5; i++) {
 printf("%d", datos[1]);
 resultado:20
 
+EJERCICIO
+#include <stdio.h>
+void insertarValor(int arr[], int pos, int valor) {
+    arr[pos] = valor;
+}
+int main() {
+    int numeros[5] = {1, 2, 3, 4, 5};
+    insertarValor(numeros, 2, 99);
+    printf("%d\n", numeros[2]);
+    return 0;
+}
+RESULTADO: 99
+
+EJERCICIO
+El siguiente programa utiliza dos funciones: una para calcular el cuadrado y otra para sumar dos cuadrados.
+#include <stdio.h>
+int cuadrado(int x) { return x * x; }
+int sumaCuadrados(int a, int b) {
+   return nose;
+}
+int main() {
+   int resultado =       sumaCuadrados( 2, 3 );
+   printf("Resultado: %d\n", resultado);
+   return 0;
+}
+¿Cuál es el resultado?
+resultado: 13
+
+EJERCICIO
+La siguiente función imprime un mensaje pero no devuelve valor. Completá el código y seleccioná la afirmación correcta sobre su
+comportamiento.
+#include <stdio.h>
+void saludar() {
+printf("Hola mundo\n");
+return ;//no es return 0,puede ser vacio
+}
+int main() {
+saludar();
+
+return 0;
+}
+¿Qué hace el programa?
+imprime hola mundo
+
+EJERCICIO
+Analiza el siguiente código:
+int matriz[2][2];
+matriz[0][0] = 1;
+matriz[0][1] = 2;
+matriz[1][0] = 3;
+matriz[1][1] = 4;
+¿Qué valores tiene la matriz después de la INSERCION?
+RESULTADO: 1 2 3 4
+
+EJERCICIO
+int numeros[] = {10, 20, 30, 40, 50};
+for (int i = 0; i < 5; i++) {
+if (i == 2) {
+numeros[i] = 100;
+}
+}
+printf("%d", numeros[2]);
+¿Qué valor se imprimirá al final del programa?
+resultado:100
+    
 DATOS:
 sizeof:sirve para saber cuanto espacio ocupa una variable o un tipo de dato en la memoria.
 general:
@@ -84,22 +149,48 @@ sizeof(arreglo[0]): tamaño de elemento
 sizeof(arreglo):dependiendo el tipo,se multiplica por la cantidad de elementos.
 ej: int arreglo[5] seria 5(elementos)*4(tipo int)=20 bytes
 
-CONCEPTOS
+PREGUNTA
 La búsqueda de un elemento en un arreglo consiste en: 
 Recorrer el arreglo a fin de encontrar el valor buscado
 
+PREGUNTA
 Un arreglo es “un conjunto finito ordenado de elementos homogéneos”.
 Ordenado:entendemos que los elementos están dispuestos de tal manera que hay un elemento cero, un elemento primero, un segundo
 Homogeneo:entendemos que todos los elementos del arreglo son del mismo tipo.
 Finito:entendemos que hay un número especifico de elementos en el arreglo; número que debe ser grande o pequeño
 
+PREGUNTA
 Una cadena no se puede ordenar aplicando cualquier método de ordenamiento que se utiliza para ordenar un arreglo:verdadero
 
+PREGUNTA
+Establecer las relaciones según corresponda:
+El ordenamiento de un arreglo 
+La búsqueda binaria no se puede aplicar 
+El arreglo debe estar ordenada  
+se realiza mediante un determinado criterio de ordenamiento
+
+PREGUNTA
+La o las ventajas de las funciones son:
+cuando cada módulo esta bien probado se lo puede usar las veces que sea necesario sin volver a revisarlo.
+
+PREGUNTA
+En C las funciones pueden ser:
+predefinidas por C y Definidas por el usuario
+
+PREGUNTA
+La sentencia RETURN fuerza la salida inmediata del cuerpo de la función y se vuelve a la siguiente sentencia  después de la llamada:
+Verdadero
+
+PREGUNTA
+Una estructura o registro es una estructura de datos que agrupa variables que pueden tener tipos diferentes: Verdadero 
+    
+PREGUNTA
 Establecer la relación entre la función y su significado:
 strcmp(): compara una cadena con otra;
 strchcr(): localiza primera instancia de un caracter dentro de un string;
 islower(caracter): devuelve cierto si caracter es una letra minúscula, y falso en caso contrario
 
+PREGUNTA
 Que proposición  no esta asociada con arreglos
 b. char <identificador> [<longitud máxima>];
 
